af9e81fd7434a5234e72654ab733aef30a61f89d
mandiseli <mandiseli@outlook.com>
2024-11-06 19:42:01 +0200

    Creating a new book for the book store

--- Bookshop/src/pages/CreateBooks.jsx
+++ Bookshop/src/pages/CreateBooks.jsx
@@ -30,7 +30,43 @@ const CreateBooks = () => {
     })
   }
   return (
-    <div>CreateBooks</div>
+    <div className='p-4'>
+      <BackButton/>
+      <h1 className='text-3x1 my-4'>Create Book</h1>
+      {loading ? <Spinner/> : ''}
+      <div className='flex flex-col border-222 border-sky-400 rounded-x1 w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-x1 mr-4 text-gray-500'>Title</label>
+          <input 
+           type="text" 
+           value={title}
+           onChange={(e) => setTitle(e.target.value)}
+           className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-x1 mr-4 text-gray-500'>Author</label>
+          <input 
+           type="text" 
+           value={author}
+           onChange={(e) => setAuthor(e.target.value)}
+           className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-x1 mr-4 text-gray-500'>Publish Year</label>
+          <input 
+           type="text" 
+           value={publishYear}
+           onChange={(e) => setPublishYear(e.target.value)}
+           className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>
+          Save
+        </button>
+      </div>
+    </div>
   )
 }
 
